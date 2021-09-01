36fc863364ab8b0fd7fc1e41c3c7e891550ed857
flopezcardozo <flopezcardozo@gmail.com>
2021-09-01 16:50:16 +0000

    Trabajo terminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-import { NULL } from "node-sass";
 import "../style/index.scss";
 
 /**
@@ -29,29 +28,55 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
   let name = "nombre";
   if (variables.name !== null) {
     name = variables.name;
   }
+
   let lastname = "apellido";
   if (variables.apellido !== null) {
     lastname = variables.apellido;
   }
+
   let socialMediaPosition = "posicionRS";
   if (variables.posicionRS !== "position-left") {
     socialMediaPosition = variables.posicionRS;
   }
+
   let role = "profesion";
   if (variables.role !== null) {
     role = variables.profesion;
   }
+
   let twitter = "redTwitter";
   if (variables.twitter !== null) {
     twitter = variables.redTwitter;
   }
+
   let github = "redGit";
   if (variables.github !== null) {
     github = variables.redGit;
+  }
+
+  let linkedin = "redLinkedin";
+  if (variables.linkedin !== null) {
+    linkedin = variables.redLinkedin;
+  }
+
+  let instagram = "redInstagram";
+  if (variables.instagram !== null) {
+    instagram = variables.redInstagram;
+  }
+
+  let country = "pais";
+  if (variables.country !== null) {
+    country = variables.pais;
+  }
+
+  let city = "ciudad";
+  if (variables.city !== null) {
+    city = variables.ciudad;
   }
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
@@ -59,12 +84,12 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
-          <h3>Miami, USA</h3>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
